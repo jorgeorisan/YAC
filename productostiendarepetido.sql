22204b992f818df7c50790f7428785ed42d8dede
jorgeorisan <jorge@illumant.com>
2018-08-19 16:55:24 -0500

    logo

--- productostiendarepetido.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM xqwmrfeeug.producto_tienda where id_producto=1614
-
-select count(id_productotienda) total, id_producto,tienda_id_tienda
-from producto_tienda 
-group by id_producto,tienda_id_tienda
-having  count(id_productotienda)>1
-
-select * from producto where id_producto=1119
