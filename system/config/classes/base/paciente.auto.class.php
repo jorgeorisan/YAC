af5fad445ac1ea9103382b46a2a0cba3f67e1b33
Yorch <jorge@illumant.com>
2019-07-02 15:44:41 -0500

    permisos

---
+++ system/config/classes/base/paciente.auto.class.php
@@ -0,0 +1,97 @@
+<?php 
+
+	class AutoPaciente {
+
+	// Variables
+		protected $db;
+		
+
+		protected $validclass = true;
+		protected $statusclass = array();
+
+
+	// Constructor Methods
+		public   function __construct(){
+			global $db;
+			$this->db = $db;
+		}
+		public static function construct( $id ){
+			$paciente = new Paciente();
+			$paciente->setId( $id );
+			return $paciente;
+		}
+		public static function constructWithValues( $values ){
+			$paciente = new Paciente();
+			$paciente->setValues( $values );
+			return $paciente;
+		}
+
+
+	// Setter Methods
+		public function setValidclass( $value ){
+			if ( $this->validclassateInput('/^(true|false)$/', ( $value ) ? 'true' : 'false', "Validclass",'s') )
+				$this->validclass = $value;
+		}
+
+		public function setStatusclass( $value ){
+			if ( ! is_array($this->statusclass) ){
+				$this->statusclass=array();
+			}
+
+			$this->statusclass[] = $value;
+			$this->statusclass = array_unique($this->statusclass );
+			
+		}
+
+
+	// Getter Methods
+		public function getValidclass(){
+			return $this->validclass;
+		}
+		public function getStatusclass(){
+			return  $this->statusclass ;
+		}
+		// Private Support Functions
+		protected function validclassateInput( $pcre, $input, $field , $bind_type) {
+			//if ( ! $this->validclass )
+			//	return $this->validclass;
+
+			if ( ! preg_match($pcre, $input) ){ 
+				return $this->killInvalidclass( "The input provided for the field '$field' is not validclass. Value provided: ".htmlentities($input),$field);
+			}else{
+				unset($this->statusclass[$field]);
+				if (empty($this->statusclass)){$this->validclass=true;}
+			}
+
+			return true;
+		}
+		protected function killInvalidclass( $msg, $field="General Error" ){
+			$this->statusclass[$field] = $msg;
+			$this->validclass = false;
+			return false;
+		}
+
+	// Public Support Functions
+		public function load($id) {
+			$sql="SELECT * FROM paciente WHERE id = ?";
+
+			if ( $id == 0 )
+				return $this->killInvalidclass( "The ID not validclass." );
+
+			// Get data 
+			$stmt = $this->db->prepare( $sql );
+			$stmt->mbind_param( 'i', $id );
+			$stmt->execute();
+
+			$res = $stmt->get_result();
+			$res = ( is_null($res) || ! $res )? [] : $res->fetch_array(MYSQLI_ASSOC) ;
+			$stmt->close();
+			if ( sizeof( $res ) == 0 ) {
+				return $this->killInvalidclass( "Unable to retrieve information for ID. Please try again later, or contact support." );
+			}
+
+			return true;
+		}
+		// end function load
+
+}
